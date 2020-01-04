db2dcea2e09e2c09d74bf917d512a9ded077914b
Brycen J Martin <bmartin22@my.whitworth.edu>
2020-01-03 22:02:42 -0800

    Homework 1 finished. Ready for review with matt to see if final touches are needed

---
+++ EX_01/EX_01/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Finds preferred name of individual
+            Console.WriteLine("I see you are known as {0} what would you like me to call you instead?", Environment.UserName);
+            string Name = Console.ReadLine();
+
+            //Takes name and finds out how old user is
+            Console.WriteLine(Name + " How old are you?");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            //determines color of background
+            if (Age < 18)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                
+            }
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            //Write Hello <name>
+            Console.WriteLine("Hello," + Name + "!");
+
+            //Message box + beep if its a minor
+            String Warning = "Danger, " + Name + " Robinson! Danger!";
+            MessageBox.Show(Warning);
+            if (Age < 18)
+            {
+                Console.Beep();
+            }
+
+            //end code
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EX_02/EX_02/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //opening setup for calculator
+            Console.WriteLine("Welcome to your calculator!");
+            Calculator c = new Calculator();
+            Boolean CalcOn = true;
+            do
+            {
+                //determines what form of arithmetic we are doing
+                Console.WriteLine("What would you like to do today? Add, Subtract, Multiply or Divide");
+                String choice = Console.ReadLine().ToLower();
+
+                //gets two integers from the user
+                Console.WriteLine("Give me two numbers!");
+                int x = Convert.ToInt32(Console.ReadLine());
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                // determines what functions to use
+                if (choice.Contains("add"))
+                {
+                    c.add(x, y);
+                }
+
+                if (choice.Contains("subtract"))
+                {
+                    c.subtract(x, y);
+                }
+                if (choice.Contains("multiply"))
+                {
+                    c.multiply(x, y);
+                }
+                if (choice.Contains("divide"))
+                {
+                    c.divide(x, y);
+                }
+
+                // asks if user wants to leave or not
+                Console.WriteLine("Would you like to calculate again?");
+                string state = Console.ReadLine();
+                if (state.Contains("no"))
+                {
+                    CalcOn = false;
+                }
+            } while (CalcOn == true);
+        }
+    }
+
+    class Calculator
+    {
+        //classes for arithmetic
+        public double add(int x, int y)
+        {
+            MessageBox.Show((x + y).ToString());
+            return x + y;
+        }
+        public double subtract(int x, int y)
+        {
+            MessageBox.Show((x - y).ToString());
+            return x - y;
+        }
+        public double multiply(int x, int y)
+        {
+            MessageBox.Show((x * y).ToString());
+            return x * y;
+        }
+        public double divide(int x, int y)
+        {
+            MessageBox.Show((x / y).ToString());
+            return x / y;
+        }
+    }
+}
